25903c94532613de95ea23c1b3f29a82dab3ad86
Kamil Rybiński <kamil.rybinski@quantumlab.co>
2018-09-04 11:05:11 +0200

    Used react-loadable instead of AsyncComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import Loadable from 'react-loadable';
 
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
+// import AsyncComponent from './components/AsyncComponent';
+
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -18,13 +20,23 @@ class App extends Component {
   }
 
   render() {
+    const Loading = () => <div>Loading...</div>;
+
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
     } else if (this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      const AsyncPage2 = Loadable({
+        loader: () => import('./components/Page2'),
+        loading: Loading
+      });
+
       return <AsyncPage2 onRouteChange={this.onRouteChange} />;
     } else if (this.state.route === 'page3') {
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      const AsyncPage3 = Loadable({
+        loader: () => import('./components/Page3'),
+        loading: Loading
+      });
+
       return <AsyncPage3 onRouteChange={this.onRouteChange} />;
     }
   }
